75bba596aef93bfe5da1d63613be9556cc8e1e4b
myungho <skqoaudgh2@hanmail.net>
2019-03-25 21:57:38 +0900

    [Update] multi-clients chat with multi-thread.

--- one-thread/client/chatclient/chatclient/client.cpp
+++ one-thread/client/chatclient/chatclient/client.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 #include <WS2tcpip.h>
+#include <thread>
 #pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
+
+void recv_socket(SOCKET sock);
 
 int main()
 {
@@ -45,11 +48,81 @@ int main()
 		return 1;
 	}
 
+	// thread to receive data
+	thread recv_thread(recv_socket, sock);
+
+	// Do-while loop to send data
+	string userInput;
+	do
+	{
+		// Prompt the user for some text
+		getline(cin, userInput);
+		if (userInput.size() > 0) // Make sure the user has typed in something
+		{
+			// Send the text
+			send(sock, userInput.c_str(), userInput.size() + 1, 0);
+		}
+	} while (userInput.size() > 0);
+
+	// Gracefully close down everything
+	recv_thread.join();
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
+
+void recv_socket(SOCKET sock)
+{
+	char buf[4096];
+	while (1)
+	{
+		ZeroMemory(buf, 4096);
+		int bytesReceived = recv(sock, buf, 4096, 0);
+		if (bytesReceived > 0)
+		{
+			cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+		}
+	}
+}
+
+/*
+	// Do-while loop to send and receive data
 	char buf[4096];
 	string userInput;
 
-	fd_set master;
+	int welcome = recv(sock, buf, 4096, 0);
+	if (welcome > 0)
+	{
+		// Echo response to console
+		cout << "SERVER> " << string(buf, 0, welcome) << endl;
+	}
+	do
+	{
+		// Prompt the user for some text
+		cout << "> ";
+		getline(cin, userInput);
+		if (userInput.size() > 0) // Make sure the user has typed in something
+		{
+			// Send the text
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR)
+			{
+				// Wait for response
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(sock, buf, 4096, 0);
+				if (bytesReceived > 0)
+				{
+					// Echo response to console
+					cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+				}
+			}
+		}
+	} while (userInput.size() > 0);
 
+*/
+
+/*
+	// Echo client-server
 	// while loop to send and receive data
 	while (1)
 	{
@@ -87,45 +160,4 @@ int main()
 			}
 		}
 	}
-
-	// Gracefully close down everything
-	closesocket(sock);
-	WSACleanup();
-	return 0;
-}
-
-/*
-	// Do-while loop to send and receive data
-	char buf[4096];
-	string userInput;
-
-	int welcome = recv(sock, buf, 4096, 0);
-	if (welcome > 0)
-	{
-		// Echo response to console
-		cout << "SERVER> " << string(buf, 0, welcome) << endl;
-	}
-	do
-	{
-		// Prompt the user for some text
-		cout << "> ";
-		getline(cin, userInput);
-		if (userInput.size() > 0) // Make sure the user has typed in something
-		{
-			// Send the text
-			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
-			if (sendResult != SOCKET_ERROR)
-			{
-				// Wait for response
-				ZeroMemory(buf, 4096);
-				int bytesReceived = recv(sock, buf, 4096, 0);
-				if (bytesReceived > 0)
-				{
-					// Echo response to console
-					cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
-				}
-			}
-		}
-	} while (userInput.size() > 0);
-
 */
